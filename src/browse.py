1ef36de795199b875f9797f1824576d104aa4174
tuomas56 <pigworts2@gmail.com>
2015-11-14 19:48:52 +0000

    improved gui

--- src/browse.py
+++ src/browse.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+
 import tkinter
 import painting
-import main
+import main as m
 import networking
 
 class CanvasImage(painting.Image):
@@ -17,13 +19,52 @@ class CanvasImage(painting.Image):
 
 def main():
 	top = tkinter.Tk()
+	top.title("Browser")
 	canvas = tkinter.Canvas(top, width=500, height=500)
+	html, css = '', ''
 	def do_go_button():
+		nonlocal html, css
 		html, css = networking.get_page(address.get())
-		image = main.render_to_image(htsrml, css, 500, 500, CanvasImage)
+		do_render()
+
+	def do_resize(event):
+		do_render()
+
+	def do_mouse_enter(event):
+		do_render()
+
+	def do_mouse_leave(event):
+		do_render()
+
+	def do_key(event):
+		do_render()
+
+	def do_left_mouse(event):
+		do_render()
+
+	def do_right_mouse(event):
+		do_render()
+
+	def do_render():
+		nonlocal html, css, canvas
+		image = m.render_to_image(html, css, canvas.winfo_height(), canvas.winfo_width(), CanvasImage)
+		canvas.delete("all")
 		image.dump(canvas)
-	go_button = tkinter.Button(top, command=do_go_button)
-	go_button.pack(side=tkinter.RIGHT)
-	address = tkinter.Entry(top)
-	address.pack(side=tkinter.LEFT)
-	canvas.pack()
+
+	frame = tkinter.Frame(top)
+	address = tkinter.Entry(frame)
+	address.pack(side="left", expand=True, fill="x")
+	go_button = tkinter.Button(frame, command=do_go_button, text="Go")
+	go_button.pack(side="right")
+	frame.pack(fill="x")
+	canvas.pack(fill="both", expand=True)
+	canvas.bind("<Configure>", do_resize)
+	canvas.bind("<Key>", do_key)
+	canvas.bind("<Button-1>", do_left_mouse)
+	canvas.bind("<Button-2>", do_right_mouse)
+	canvas.bind("<Enter>", do_mouse_enter)
+	canvas.bind("<Leave>", do_mouse_leave)
+	top.mainloop()
+
+if __name__ == "__main__":
+	main()
